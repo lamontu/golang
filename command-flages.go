226a0f1544edaa60564c72544b53631bd6f30507
lamontu <lamontyu@163.com>
2017-11-27 13:29:42 +0800

    Test command line flags

---
+++ command-flages.go
@@ -0,0 +1,26 @@
+// ./command-flages
+// ./command-flages -h
+// ./command-flages -word=opt
+// ./command-flages -word=opt fork=true a b c
+
+package main
+
+import "flag"
+import "fmt"
+
+func main() {
+    wordPtr := flag.String("word", "foo", "a string")
+    numbPtr := flag.Int("numb", 42, "an int")
+    boolPtr := flag.Bool("fork", false, "a bool")
+
+    var svar string
+    flag.StringVar(&svar, "svar", "bar", "a string var")
+
+    flag.Parse()
+
+    fmt.Println("word:", *wordPtr)
+    fmt.Println("numb:", *numbPtr)
+    fmt.Println("fork:", *boolPtr)
+    fmt.Println("svar:", svar)
+    fmt.Println("tail:", flag.Args())
+}
