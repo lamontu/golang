9d9e1ed914d7b61fc049f0d982aef659e96cb86e
lamontu <lamontyu@163.com>
2017-11-27 11:46:56 +0800

    Test command line arguments

---
+++ command-arguments.go
@@ -0,0 +1,18 @@
+// go build command-arguments.go
+// ./command-arguments a b c d
+
+package main
+
+import "os"
+import "fmt"
+
+func main() {
+    argsWithProg := os.Args
+    argsWithoutProg := os.Args[1:]
+
+    arg := os.Args[3]
+
+    fmt.Println(argsWithProg)
+    fmt.Println(argsWithoutProg)
+    fmt.Println(arg)
+}
