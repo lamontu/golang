428a66d3a2de6e26025d869422fcdaecf2c61a93
lamontu <lamontyu@163.com>
2018-03-06 15:06:25 +0800

    Test length and capacity of slices

---
+++ slice-len-cap.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
+
+    //ss := s[0:3:3]
+	//ss := s[0:3:5]
+    ss := s[1:3:6]
+	printSlice(ss)
+
+	// Slice the slice to give it zero length.
+	s = s[:0]
+	printSlice(s)
+
+	// Extend its length.
+	s = s[:4]
+	printSlice(s)
+
+	// Drop its first two values.
+	s = s[2:]
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
